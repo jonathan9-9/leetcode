d79f1cd8106c40acb7b1485fc777a56c2119668c
Jonathan <jonathancornejox@gmail.com>
2023-10-27 00:00:46 -0700

    oop

--- classes/OOP.js
+++ classes/OOP.js
@@ -37,3 +37,34 @@ const hamster1 = new Hamster("Squishy", "Squeak!");
 
 console.log(dog1.name); // the output will be: "Oreo"
 cat1.nineLives(); // output: I have nine lives
+
+class Animal {
+  constructor(speed) {
+    this.speed = speed;
+  }
+  move = () => console.log(`I am moving at the speed of ${this.speed}`);
+}
+
+// parent class
+class Companion extends Animal {
+  constructor(love, speed) {
+    super(speed);
+    this.love = love;
+  }
+  affection = () => console.log(`My level of affection is ${this.love}`);
+}
+
+class Cat extends Companion {
+  constructor(name, phrase, love, speed) {
+    super(love, speed);
+    this.name = name;
+    this.phrase = phrase;
+    this.species = "cat";
+  }
+  nineLives = () => console.log("I have nine lives");
+  sayPhrase = () => console.log(this.phrase);
+}
+
+const cat3 = new Cat("Milo", "Meow", 8);
+cat3.affection(); // Output: My level of affection is 8
+console.log(cat3.love); // Output: 8
