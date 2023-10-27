4364c9c6d854447835c5e96448c4476af133f902
Jonathan <jonathancornejox@gmail.com>
2023-10-27 00:26:40 -0700

    oop updates on inheritance chain

--- classes/OOP.js
+++ classes/OOP.js
@@ -1,33 +1,50 @@
-class Dog {
-  // constructor function
-  constructor(name, phrase) {
-    this.name = name; // properties (characteristics)
-    this.phrase = phrase;
-    this.species = "dog";
+// Grandparent class
+class Animal {
+  constructor(speed) {
+    this.speed = speed;
   }
-
-  bark = () => console.log("woof, woof!");
-  sayPhrase = () => console.log(this.phrase); // method (action)
+  move = () => console.log(`I am moving at the speed of ${this.speed}`);
 }
 
-class Cat {
-  constructor(name, phrase) {
+// parent class
+class Companion extends Animal {
+  constructor(name, phrase, love, speed) {
+    super(name, phrase, love, speed);
     this.name = name;
     this.phrase = phrase;
+    this.love = love;
+  }
+  affection = () => console.log(`My level of affection is ${this.love}`);
+}
+
+// Cat is now the "child class"
+class Cat extends Companion {
+  constructor(name, phrase, love, speed) {
+    super(name, phrase, love, speed);
     this.species = "cat";
   }
   nineLives = () => console.log("I have nine lives");
   sayPhrase = () => console.log(this.phrase);
 }
 
-class Hamster {
-  constructor(name, phrase) {
-    this.name = name;
-    this.phrase = phrase;
+class Hamster extends Companion {
+  constructor(name, phrase, love, speed) {
+    super(name, phrase, love, speed);
     this.species = "hamster";
   }
   silent = () => console.log("I am a silent animal");
   sayPhrase = () => console.log(this.phrase);
+}
+
+class Dog extends Companion {
+  // constructor function
+  constructor(name, phrase, love, speed) {
+    super(name, phrase, love, speed);
+    this.species = "dog"; // property (characteristic)
+  }
+
+  bark = () => console.log("woof, woof!");
+  sayPhrase = () => console.log(this.phrase); // method (action)
 }
 
 const dog1 = new Dog("Oreo", "I am Oreo the dog!");
@@ -38,33 +55,10 @@ const hamster1 = new Hamster("Squishy", "Squeak!");
 console.log(dog1.name); // the output will be: "Oreo"
 cat1.nineLives(); // output: I have nine lives
 
-class Animal {
-  constructor(speed) {
-    this.speed = speed;
-  }
-  move = () => console.log(`I am moving at the speed of ${this.speed}`);
-}
-
-// parent class
-class Companion extends Animal {
-  constructor(love, speed) {
-    super(speed);
-    this.love = love;
-  }
-  affection = () => console.log(`My level of affection is ${this.love}`);
-}
-
-class Cat extends Companion {
-  constructor(name, phrase, love, speed) {
-    super(love, speed);
-    this.name = name;
-    this.phrase = phrase;
-    this.species = "cat";
-  }
-  nineLives = () => console.log("I have nine lives");
-  sayPhrase = () => console.log(this.phrase);
-}
-
 const cat3 = new Cat("Milo", "Meow", 8);
 cat3.affection(); // Output: My level of affection is 8
 console.log(cat3.love); // Output: 8
+
+const cat4 = new Cat("Milo", "Meow", 10, 70);
+cat4.move(); // output: I am moving at the speed of 70
+console.log(cat4.speed); //output: 70
