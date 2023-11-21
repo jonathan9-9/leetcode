36cc6b7d9f592b6cd2619b813f425c07064fa96c
Jonathan <jonathancornejox@gmail.com>
2023-11-20 21:10:47 -0800

    valid palindrome

---
+++ valid-palindrome/11-20-23-vp.js
@@ -0,0 +1,37 @@
+var isPalindrome = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  if (s == null) return false;
+
+  s = s.toLowerCase();
+
+  while (left < right) {
+    while (
+      left < right &&
+      !(
+        (s[left] >= "a" && s[left] <= "z") ||
+        (s[left] >= "0" && s[left] <= "9")
+      )
+    ) {
+      left++;
+    }
+    while (
+      left < right &&
+      !(
+        (s[right] >= "a" && s[right] <= "z") ||
+        (s[right] >= "0" && s[right] <= "9")
+      )
+    ) {
+      right--;
+    }
+
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+};
