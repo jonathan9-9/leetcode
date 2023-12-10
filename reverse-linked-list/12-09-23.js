10c410c4bb7651769adf06e9b9c2e5a7e7fe9e54
Jonathan <jonathancornejox@gmail.com>
2023-12-09 19:25:52 -0800

    comments

--- reverse-linked-list/12-09-23.js
+++ reverse-linked-list/12-09-23.js
@@ -14,12 +14,17 @@ var reverseList = function (head) {
   let next = null;
 
   while (head) {
+    // stores the reference to the next node in the original list
     next = head.next;
 
+    // moves pointer to the previous node to reverse the list
     head.next = prev;
 
+    // previous node now points to the new head of the reversed list
     prev = head;
 
+    // head is assigned to next or (head.next) in order to traverse the original list
+    // the next variable stores head.next so in other words head = head.next
     head = next;
   }
 
