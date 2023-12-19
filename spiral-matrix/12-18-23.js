4730ffc1a18ddef4c9dacbed017e45afee60c40b
Jonathan <jonathancornejox@gmail.com>
2023-12-18 21:19:50 -0800

    comments

--- spiral-matrix/12-18-23.js
+++ spiral-matrix/12-18-23.js
@@ -5,19 +5,23 @@
 var spiralOrder = function (matrix) {
   const result = [];
 
+  // left to right
   while (matrix.length > 0) {
     result.push(...matrix.shift());
 
+    // top to bottom
     for (let row of matrix) {
       if (row.length > 0) {
         result.push(row.pop());
       }
     }
 
+    // right to left
     if (matrix.length > 0) {
       result.push(...matrix.pop().reverse());
     }
 
+    // bottom to top
     for (let i = matrix.length - 1; i >= 0; i--) {
       if (matrix[i].length > 0) {
         result.push(matrix[i].shift());
