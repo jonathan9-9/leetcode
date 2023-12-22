ceeaace43cfa020dbbdc1edcaf3302dbdf249939
Jonathan <jonathancornejox@gmail.com>
2023-12-22 14:08:41 -0800

    matrix zeroes

---
+++ set-matrix-zeroes/12-22-23.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function (matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  let firstRowHasZero = false;
+
+  for (let j = 0; j < cols; j++) {
+    if (matrix[0][j] === 0) {
+      firstRowHasZero = true;
+      break;
+    }
+  }
+
+  for (let i = 1; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[i][0] = 0;
+        matrix[0][j] = 0;
+      }
+    }
+  }
+
+  for (let i = 1; i < rows; i++) {
+    if (matrix[i][0] === 0) {
+      for (let j = 0; j < cols; j++) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  for (let j = 0; j < cols; j++) {
+    if (matrix[0][j] === 0) {
+      for (let i = 0; i < rows; i++) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  if (firstRowHasZero) {
+    for (let j = 0; j < cols; j++) {
+      matrix[0][j] = 0;
+    }
+  }
+};
