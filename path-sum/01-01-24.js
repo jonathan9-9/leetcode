fa387241ae1cbd39927459069dc400650b961343
Jonathan <jonathancornejox@gmail.com>
2024-01-01 13:14:01 -0800

    path sum

---
+++ path-sum/01-01-24.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+  if (!root) return false;
+
+  if (!root.right && !root.left && root.val === targetSum) return true;
+
+  return (
+    hasPathSum(root.right, targetSum - root.val) ||
+    hasPathSum(root.left, targetSum - root.val)
+  );
+};
