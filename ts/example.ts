d845bae3ca817e0185c5d9f6d8b9a7b813c127d9
Jonathan <jonathancornejox@gmail.com>
2024-01-05 20:21:26 -0800

    ts

---
+++ ts/example.ts
@@ -0,0 +1,20 @@
+type Musician1 = {
+  name: string;
+  age: string | number;
+  dateOfBirth: number;
+  isPopular?: boolean;
+};
+
+type Musician2 = {
+  name: string;
+  age: string | number;
+  dateOfBirth: number;
+  isPopular?: boolean;
+};
+
+interface Musician3 {
+  name: string;
+  age: string | number;
+  dateOfBirth: number;
+  isPopular?: boolean;
+}
