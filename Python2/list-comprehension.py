3298f82e5493b2d565a7031fd5147957968f74c5
Jonathan <jonathancornejox@gmail.com>
2024-03-30 21:08:03 -0700

    fibonacci sequence

--- Python2/list-comprehension.py
+++ Python2/list-comprehension.py
@@ -1,8 +1,21 @@
-import math
+# import math
 
 
-nums = [16, 64, 144]
+# nums = [16, 64, 144]
 
-square_root_nums = list(map(lambda x: math.sqrt(x), nums))
+# square_root_nums = list(map(lambda x: math.sqrt(x), nums))
 
-print("square root of nums", square_root_nums)
+# print("square root of nums", square_root_nums)
+
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+# Test the function
+print("Fibonacci sequence:")
+for i in range(10):
+    print(fibonacci(i))
