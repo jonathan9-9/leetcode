212327deff9c512d31aa0c6cb1df1a883db5f816
Jonathan <jonathancornejox@gmail.com>
2024-04-05 21:04:23 -0700

    second solution to rotate image

--- Python/03-31-24-rotate-image.py
+++ Python/03-31-24-rotate-image.py
@@ -17,3 +17,37 @@ class Solution:
         for i in range(len(matrix)):
             for j in range(i):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+# Second solution: more efficient and intuitive
+
+
+class RotateImage:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        l, r = 0, len(matrix) - 1
+
+        while l < r:
+            for i in range(r - l):
+
+                top, bottom = l, r
+
+                topLeft = matrix[top][l + i]
+
+                # move bottom left into top left
+                matrix[top][l + i] = matrix[bottom - i][l]
+
+                # move bottom right into bottom left
+                matrix[bottom - i][l] = matrix[bottom][r - i]
+
+                # move top right into bottom right
+                matrix[bottom][r - i] = matrix[top + i][r]
+
+                # move top left (variable that stores top left) into top right
+                matrix[top + i][r] = topLeft
+
+            r -= 1
+            l += 1
