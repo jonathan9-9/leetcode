f7d4cb66d36c71f3b291e9cbd0c56b6ce4fc79b4
Jonathan <jonathancornejox@gmail.com>
2024-04-05 20:10:32 -0700

    combinations sum ii lc my solution

--- Python/04-04-24-combinations-sum-II.py
+++ Python/04-04-24-combinations-sum-II.py
@@ -2,22 +2,36 @@ from typing import List
 
 
 class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum2(
+        self, candidates: List[int], target: int
+    ) -> List[List[int]]:  # noqa: E501
 
         result = []
-        combinations = []
-
         candidates.sort()
 
-        if target < 0:
-            return
+        def backtrack(start, candidates, combinations, target):
+            if target < 0:
+                return
 
-        if target == 0:
-            combinations.append(x for x in combinations)
-
-        def backtrack(start, candidates, combinatons):
+            if target == 0:
+                # create copy of current combinations array
+                result.append(combinations[:])
+                return
             for i in range(start, len(candidates)):
-                if start > i and candidates[i] == candidates[i - 1]:
+                # avoid duplicate combinations: skip num
+                if i > start and candidates[i] == candidates[i - 1]:
                     continue
+                combinations.append(candidates[i])
+                backtrack(
+                    i + 1, candidates, combinations, target - candidates[i]
+                )  # noqa
+                combinations.pop()
 
-        backtrack(0, candidates, [])
+        backtrack(0, candidates, [], target)
+
+        return result
+
+
+# [3,2,5,1,5,2,6,4] target = 7
+
+# result = [[2,5], [1,6], [3,4]]
