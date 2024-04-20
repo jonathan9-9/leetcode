ba5a7ab0bc0fb3f0971590944534c6834d18cf67
Jonathan <jonathancornejox@gmail.com>
2024-04-20 13:08:09 -0700

    dir structure

--- Python/Arrays/04-14-24-squares-of-a-sorted-arr.py
+++ Python/Arrays/04-14-24-squares-of-a-sorted-arr.py
@@ -21,6 +21,10 @@ class Solution:
         return result
 
 
+# nums = [-4, -1, 0, 3, 10]
+# output = [0, 1, 9, 16, 100]
+
+
 # class Solution:
 #     def sortedSquares(self, nums: List[int]) -> List[int]:
 
