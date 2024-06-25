61b4e180942e84c2f2ebdc5192ce99dfd73b671a
Jonathan <jonathancornejox@gmail.com>
2024-06-24 19:46:36 -0700

    driver program for Book class

--- Java/Book.java
+++ Java/Book.java
@@ -1,5 +1,7 @@
 public class Book {
 
+    private static final int LONG_BOOK_PAGE_NUMBER = 1000;
+
     private String bookTitle;
     private String author;
     private int numPages;
@@ -20,7 +22,7 @@ public class Book {
 
     public void setNumPages(int numBookPages) {
         if (numBookPages < 0) {
-            throw new IllegalArgumentException("Only numbers are allowed; no letters or symbols allowed.");
+            throw new IllegalArgumentException("Only positive numbers are allowed; no letters or symbols allowed.");
         } else {
             numPages = numBookPages;
         }
@@ -48,10 +50,26 @@ public class Book {
         return isCurrentBookLong;
     }
 
+    public int countAuthors(String strOfAuthors) {
+        int count = 0;
+
+        String[] listAuthors = strOfAuthors.split("&");
+        for (int i = 0; i < listAuthors.length; i++) {
+            if (listAuthors[i] != null) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean isBookLong() {
+        return isBookLong(LONG_BOOK_PAGE_NUMBER);
+    }
+
     public String toString(String str) {
         return "The book of " + getBookTitle() + " written by " + getBookAuthors()
                 + " is about a tale of when the land of Europe came to ruin after a major battle between two kings "
-                + " fighting for power and sovereignty. This battle was ongoing for decades until one power united and killed thousands of people and severely damages infrastructure";
+                + " fighting for power and sovereignty. This battle was ongoing for decades until one power united and killed thousands of people and severely damaged infrastructure";
     }
 
 }
---
+++ Java/Driver.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Driver {
+    public static void main(String[] args) {
+
+        Book book1 = new Book(null, null, 0);
+        Book book2 = new Book(null, null, 0);
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter book title #one: ");
+        String userBookTitle1 = scanner.nextLine();
+
+        System.out.println("Enter author(s) of book #one: ");
+        String userBookAuthor1 = scanner.nextLine();
+
+        System.out.println("Enter number of pages of book #one: ");
+        int numPagesBook1 = Integer.parseInt(scanner.nextLine());
+
+        book1.setTitle(userBookTitle1);
+        book1.setAuthor(userBookAuthor1);
+
+        System.out.println(book1.toString());
+
+        System.out.println("Enter author(s) of book #two: ");
+        String userBookAuthor2 = scanner.nextLine();
+        book2.setAuthor(userBookAuthor2);
+        int numAuthors = book2.countAuthors(userBookAuthor2);
+        System.out.println("Book #two author count: " + numAuthors);
+
+        System.out.println(book1.isBookLong(numPagesBook1));
+
+    }
+}
