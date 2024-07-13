c818f993482b3e254274d31fbdb4279044fd64f5
Jonathan <jonathancornejox@gmail.com>
2024-07-13 12:10:57 -0700

    part class with getter method get guests

--- Java/Party.java
+++ Java/Party.java
@@ -1,5 +1,21 @@
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Iterator;
 
 public class Party {
+    private LocalDateTime date;
+    private Set<String> guests;
 
+    public Party(LocalDateTime date) {
+        this.date = date;
+        this.guests = new HashSet<>();
+    }
+
+    public List<String> getGuests() {
+        return new ArrayList<>(guests);
+    }
 }
