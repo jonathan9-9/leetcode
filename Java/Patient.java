09067eabe4a99c3a6c9a107dbf14b02111c6fa58
Jonathan <jonathancornejox@gmail.com>
2024-07-14 14:24:45 -0700

    patient class getters and setters and overloaded patient constructor

--- Java/Patient.java
+++ Java/Patient.java
@@ -11,4 +11,32 @@ public class Patient {
         this.visitList = new ArrayList<DoctorVisit>();
     }
 
+    public Patient(int patientID, String name, DoctorVisit firstVisit) {
+        this(patientID, name);
+        this.visitList.add(firstVisit);
+    }
+
+    public int getPatientID() {
+        return this.id;
+    }
+
+    public String getPatientName() {
+        return this.name;
+    }
+
+    public List<DoctorVisit> getPatientDoctorVisits() {
+        return this.visitList;
+    }
+
+    public void setPatientID(int patientID) {
+        this.id = patientID;
+    }
+
+    public void setPatientName(String patientName) {
+        this.name = patientName;
+    }
+
+    public void addDoctorVisit(DoctorVisit visit) {
+        this.visitList.add(visit);
+    }
 }
