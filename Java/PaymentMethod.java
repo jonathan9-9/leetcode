d71c136f0dbbd6f862fa9c3bea9261620c913cbc
Jonathan <jonathancornejox@gmail.com>
2024-07-14 13:37:06 -0700

    payment method enum getter method for patient pays and toString method

--- Java/PaymentMethod.java
+++ Java/PaymentMethod.java
@@ -1,3 +1,29 @@
-public class PaymentMethod {
+public enum PaymentMethod {
+    INSURANCE_PAY(false),
+    PAYMENT_PLAN(true),
+    FULL_PAY(true);
 
+    private final boolean patientPays;
+
+    PaymentMethod(boolean patientPays) {
+        this.patientPays = patientPays;
+    }
+
+    public boolean patientPays() {
+        return this.patientPays;
+    }
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case FULL_PAY:
+                return "Patient pays in full.";
+            case INSURANCE_PAY:
+                return "Patient does not pay.";
+            case PAYMENT_PLAN:
+                return "Patient pays by installments.";
+            default:
+                return "Payment method not specified.";
+        }
+    }
 }
