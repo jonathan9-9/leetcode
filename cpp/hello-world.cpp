ecbb2ccf0883045f719a328b053961a7243da452
Jonathan <jonathancornejox@gmail.com>
2024-09-27 15:02:41 -0700

    unordered map c++ program

--- cpp/hello-world.cpp
+++ cpp/hello-world.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <unordered_map>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  unordered_map<int, string> hashMap;
+
+  hashMap[1] = "Apple";
+  hashMap[2] = "Orange";
+  hashMap[3] = "Watermelon";
+
+  unordered_map<int, string>::iterator it;
+  for (it = hashMap.begin(); it != hashMap.end(); ++it) {
+    cout << "Key: " << it->first << ", Value: " << it->second << endl;
+  }
+
+  // for (const auto &pair : hashMap) {
+  //   cout << "Key: " << pair.first << ", Value: " << pair.second << endl;
+  // }
+  return 0;
 }
--- valid-parentheses/11-25-2023-valid-paren.js
+++ valid-parentheses/11-25-2023-valid-paren.js
@@ -1,11 +1,20 @@
+/* Given a string s containing just the characters '(', ')', '{', '}', '[',']'
+determine if the input string is valid. An input string is valid if:
+1. Open brackets must be closed by the same type of brackets
+2. Open brackets must be closed in the correct order
+3. Every close bracket has a corresponding open bracket of the same type
+*/
+
 var isValid = function (s) {
   const compare = "() {} []";
+
   let i = 0;
   const stack = [];
 
   while (i < s.length) {
     stack.push(s[i]);
     if (compare.includes(stack[stack.length - 2] + stack[stack.length - 1])) {
+      console.log("Stack: ", stack);
       stack.pop();
       stack.pop();
     }
@@ -14,3 +23,5 @@ var isValid = function (s) {
 
   return stack.length === 0;
 };
+
+isValid("() {} []");
