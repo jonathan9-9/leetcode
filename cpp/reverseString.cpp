7d6ea7e557bc296bdc299090001b2e3756bb1cca
Jonathan <jonathancornejox@gmail.com>
2024-10-12 21:04:40 -0700

    reverse a string c++

---
+++ cpp/reverseString.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+void swapCharacters(char &a, char &b) {
+  char temp = a;
+  a = b;
+  b = temp;
+}
+
+void reverseString(std::string &str) {
+  int left = 0;
+  int right = str.length() - 1;
+
+  while (left < right) {
+    swapCharacters(str[left], str[right]);
+    left++;
+    right--;
+  }
+}
+
+int main() {
+
+  std::string s = "giselle"; // reversed: olleh
+
+  reverseString(s);
+
+  std::cout << "Reversed string: " << s << std::endl;
+  return 0;
+}
