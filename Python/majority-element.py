5d96d91c19ab967c61a76c41b008557b011a28e5
Jonathan <jonathancornejox@gmail.com>
2024-12-23 17:48:13 -0800

    majority element counter collection example input

--- Python/majority-element.py
+++ Python/majority-element.py
@@ -5,6 +5,8 @@ from typing import List
 def majorityElement(nums: List[int]) -> int:
     max_freq = Counter(nums)
 
+    print(f"Data structure of Counter: {max_freq}")
+
     max_count = float('inf')
     num_most_seen = 0
 
@@ -12,3 +14,6 @@ def majorityElement(nums: List[int]) -> int:
         if count > max_count:
             num_most_seen = num
     return num_most_seen
+
+
+majorityElement([2, 7, 7, 7, 1, 10, 9, 9])
