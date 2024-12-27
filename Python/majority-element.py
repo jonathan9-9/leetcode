94379d4c933c28c17040212321ca61bab2afe56d
Jonathan <jonathancornejox@gmail.com>
2024-12-26 19:01:32 -0800

    more comments

--- Python/majority-element.py
+++ Python/majority-element.py
@@ -12,7 +12,7 @@ def majorityElement(nums: List[int]) -> int:
 
 
 # iterates through each numbers and its count
-# Another comment
+# More comments
     for num, count in max_freq.items():
         if count > max_count:
             num_most_seen = num
