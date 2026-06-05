bc9b63b2c854e30af9547c35265a8dea75aecd55
Jonathan <jonathancornejox@gmail.com>
2026-06-04 19:22:06 -0700

    container with most water lc problem

---
+++ Python/Arrays/Pointers/06-04-24-containerwith-mostwater.py
@@ -0,0 +1,18 @@
+
+from ast import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = 0
+        left = 0
+        right = len(height) - 1
+        while left < right:
+            min_height = min(height[left], height[right])
+            distance = right - left
+            max_area = max(max_area, min_height * distance)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_area
