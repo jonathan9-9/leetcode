f50a7830d189ce5cc95f845b1bc8893bcdff0db2
Jonathan <jonathancornejox@gmail.com>
2026-07-15 15:36:58 -0700

    depth of binary tree leetcode problem solved using BFS

---
+++ Python/BST/07-15-26-depth-of-binaryTree.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        size = 0
+        queue = deque([root])
+        while queue:
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            size += 1
+        return size
